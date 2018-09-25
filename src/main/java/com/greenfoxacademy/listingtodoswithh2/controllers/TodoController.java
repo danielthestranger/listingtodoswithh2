34459586035f2884eadf6a24dc94b0532f166c0d
danielthestranger <danielthestranger@gmail.com>
2018-09-25 14:28:59 +0200

    chore(TodoRepository): create TodoRepository CRUD JPA interface

--- src/main/java/com/greenfoxacademy/listingtodoswithh2/controllers/TodoController.java
+++ src/main/java/com/greenfoxacademy/listingtodoswithh2/controllers/TodoController.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.listingtodoswithh2.controllers;
 
+import com.greenfoxacademy.listingtodoswithh2.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
+
+    TodoRepository todoRepository;
+
+    @Autowired
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
     @GetMapping(value = {"/", "/list"})
     @ResponseBody
